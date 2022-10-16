deb02a69f422da7f36a5e09e50b076c638ebd970
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2022-10-16 21:37:40 +0300

    fix

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -11,18 +11,18 @@ export const getMongoConfig = async (
 };
 
 const getMongoString = (configService: ConfigService) => {
-  return 'mongodb://' +
+  return (
+    'mongodb://' +
     configService.get('MONGO_LOGIN') +
     ':' +
     configService.get('MONGO_PASSWORD') +
     '@' +
-    process.env.PRODUCTION
-    ? configService.get('MONGO_HOST_PRODUCTION')
-    : configService.get('MONGO_HOST_DEVELOP') +
-        ':' +
-        configService.get('MONGO_PORT') +
-        '/' +
-        configService.get('MONGO_AUTH_DB');
+    configService.get('MONGO_HOST_PRODUCTION') +
+    ':' +
+    configService.get('MONGO_PORT') +
+    '/' +
+    configService.get('MONGO_AUTH_DB')
+  );
 };
 
 const getMongoOptions = () => ({
